048c127a75417aef2a890779d9ccbab474efc02b
Sim Shang En <simshangen@gmail.com>
2020-09-03 22:03:17 +0800

    Improved NRIC Reconstructor
    Added:
    - Ability to reconstruct NRIC from n characters of NRIC with sufficient external information

--- reconstructor.py
+++ reconstructor.py
@@ -1,3 +1,6 @@
+import re
+
+
 def last_char1(val):
     switch = {
         0: 'J',
@@ -32,7 +35,7 @@ def last_char2(val):
     return switch.get(val)
 
 
-def validate_num(nric):
+def validate_nric(nric):
     x = (int(nric[1]) * 2) + (int(nric[2]) * 7) + (int(nric[3]) * 6) + (int(nric[4]) * 5) + (int(nric[5]) * 4) + (int(nric[6]) * 3) + (int(nric[7]) * 2)
 
     if nric[0] == 'T' or nric[0] == 'G':
@@ -57,69 +60,94 @@ def validate_num(nric):
 
 
 def permgen(items, n):
-    # generates the 1st 4 numbers of nric
-    if n==0: yield []
+    # generates the 1st n numbers of nric
+    if n == 0:
+        yield []
     else:
         for i in range(len(items)):
-            for cc in permgen(items,n-1):
+            for cc in permgen(items, n-1):
                 yield [items[i]]+cc
 
 
-def construct_ic(val):
-    for c in permgen(['0','1','2','3','4','5','6','7','8','9'],4):
+def construct_ic(partial_nric, n):
+    nric_set = set()
+    for c in permgen(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 7-n+1):
         for i in range(4):
             nric = []
             if i == 0:
                 nric.append('S')
                 nric.append(''.join(c))
-                nric.append(val)
-                
+                nric.append(partial_nric)
+
                 # try nric
-                success = validate_num(''.join(nric))
+                success = validate_nric(''.join(nric))
 
                 if success:
-                    f = open('nric.txt', 'a')
-                    f.write(''.join(nric) + '\n')
-                    f.close()
+                    nric_set.add(''.join(nric))
             elif i == 1:
                 nric.append('T')
                 nric.append(''.join(c))
-                nric.append(val)
+                nric.append(partial_nric)
 
-                success = validate_num(''.join(nric))
+                success = validate_nric(''.join(nric))
 
                 if success:
-                    f = open('nric.txt', 'a')
-                    f.write(''.join(nric) + '\n')
-                    f.close()
+                    nric_set.add(''.join(nric))
             elif i == 2:
                 nric.append('F')
                 nric.append(''.join(c))
-                nric.append(val)
+                nric.append(partial_nric)
 
-                success = validate_num(''.join(nric))
+                success = validate_nric(''.join(nric))
 
                 if success:
-                    f = open('nric.txt', 'a')
-                    f.write(''.join(nric) + '\n')
-                    f.close()
+                    nric_set.add(''.join(nric))
             else:
                 nric.append('G')
                 nric.append(''.join(c))
-                nric.append(val)
+                nric.append(partial_nric)
 
-                success = validate_num(''.join(nric))
+                success = validate_nric(''.join(nric))
 
                 if success:
-                    f = open('nric.txt', 'a')
-                    f.write(''.join(nric) + '\n')
-                    f.close()
+                    nric_set.add(''.join(nric))
+    return nric_set
+
+
+def filter_nric(year, citizenship, last, nric_set, location_of_birth):
+    # Singapore citizens and permanent residents born before 1 January 2000 are assigned the letter "S".
+    # Singapore citizens and permanent residents born on or after 1 January 2000 are assigned the letter "T".
+    # Foreigners issued with long-term passes before 1 January 2000 are assigned the letter "F".
+    # Foreigners issued with long-term passes on or after 1 January 2000 are assigned the letter "G".
+    # Singapore citizens and permanent residents born on or after 1 January 1968 are issued NRIC numbers starting with their year of birth, e.g. S71xxxxx# for a person born in 1971 and T02xxxxx# for a person born in 2002. For those born in Singapore, these numbers are identical to the birth registration number on their birth certificates, which are automatically transferred to the NRIC at age 15 and above.
+    if (citizenship == 'S' or citizenship == 'P') and int(year) >= 2000:
+        first_letter = 'T'
+    elif (citizenship == 'S' or citizenship == 'P') and int(year) <= 2000:
+        first_letter = 'S'
+    elif citizenship == 'F' and int(year) >= 2000:
+        first_letter = 'G'
+    else:
+        first_letter = 'F'
+    if location_of_birth == 'Y':
+        pattern = f'{first_letter}{year[2:]}[1-5]*{str(last)}'
+    else:
+        pattern = f'{first_letter}{year[2:]}[5-9]*{str(last)}'
+    r = re.compile(pattern)
+    nric_filtered = list(filter(r.match, nric_set))
+    print(f'Possible NRIC ({len(nric_filtered)})')
+    for nric in nric_filtered:
+        print(nric)
 
 
 def main():
-    # get user input
-    val = input('Enter last 4 characters of nric: ')
+    n = int(input('How many characters of the NRIC do you have?: '))
+    partial_nric = input(f'Enter last {n} characters of NRIC: ')
+    dob = input('Enter Year of Birth [YYYY]: ')
+    citizenship = input('Enter Citizenship ([P]R / [S]ingaporean / [F]oreigner): ')
+    location_of_birth = input('Is the person born in Singapore? [Y / N]: ')
+    nric_set = construct_ic(partial_nric, n)
+    filter_nric(dob, citizenship, partial_nric, nric_set, location_of_birth)
 
-    construct_ic(val)
 
-main()
+if __name__ == '__main__':
+    main()
